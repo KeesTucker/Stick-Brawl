1cb0004ba9577a90e16e69d53bbbc819ff26c0bf
AXsmasher <keeswtucker@gmail.com>
2019-07-20 14:05:37 +1200

    Gay

--- Assets/Scripts/MultiplayerStart.cs
+++ Assets/Scripts/MultiplayerStart.cs
@@ -13,7 +13,6 @@ public class MultiplayerStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.DeleteAll();
         transport = NetworkManager.singleton.gameObject.GetComponent<LiteNetLib4MirrorTransport>();
     }
 
